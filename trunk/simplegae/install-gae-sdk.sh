b676a48c61127b5bcd5950b11ea5848d5d47a3df
houman001@gmail.com <houman001@gmail.com@025b7ef8-6fa8-6c12-81c7-582cdaf5cbfc>
2011-08-18 12:23:26 +0000

    Removed .zip file

--- trunk/simplegae/install-gae-sdk.sh
+++ trunk/simplegae/install-gae-sdk.sh
@@ -1,23 +1,10 @@
-echo "Going to install AppEngine and unpack it."
-echo "Press any key to continue..."; read
-
-# STEP 1: Installation of AppEngine dependencies (version 1.4.3)
-export M2_REPO=~/.m2/repository
 export GAE_VERSION=1.4.3
 
-# Installing Google AppEngine dependencies
-
-# Install appengine-java-sdk with the following command and unzip the installed file (which is under ${M2_REPO}/com/google/appengine/appengine-java-sdk/${GAE_VERSION}/ directory)
-mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-java-sdk -Dversion=${GAE_VERSION} -Dpackaging=zip -Dfile=./files/appengine-java-sdk-${GAE_VERSION}.zip
-unzip -d ~/.m2/repository/com/google/appengine/appengine-java-sdk/${GAE_VERSION} ~/.m2/repository/com/google/appengine/appengine-java-sdk/${GAE_VERSION}/appengine-java-sdk-${GAE_VERSION}.zip
-
 echo "Going to install AppEngine dependencies."
-echo "Press any key to continue..."; read
 
 # Install appengine-local-runtime
-mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=${GAE_VERSION} -Dpackaging=jar -Dfile=${M2_REPO}/com/google/appengine/appengine-java-sdk/${GAE_VERSION}/appengine-java-sdk-${GAE_VERSION}/lib/impl/appengine-local-runtime.jar
+mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=${GAE_VERSION} -Dpackaging=jar -Dfile=./files/appengine-local-runtime-${GAE_VERSION}.jar
+mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=${GAE_VERSION} -Dpackaging=pom -Dfile=./files/appengine-local-runtime-${GAE_VERSION}.pom
 
-# Now the application is ready to be compiled and run.
-
-echo "Now is the time to compile the application."
+echo "Done."
 
