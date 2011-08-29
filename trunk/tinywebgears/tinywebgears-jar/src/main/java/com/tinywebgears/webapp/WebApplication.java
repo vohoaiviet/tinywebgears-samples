b27e674878cfa1948e619cbd0a52180da7216036
houman001@gmail.com <houman001@gmail.com@025b7ef8-6fa8-6c12-81c7-582cdaf5cbfc>
2011-08-29 06:48:59 +0000

    some modifications

--- trunk/tinywebgears/tinywebgears-jar/src/main/java/com/tinywebgears/webapp/WebApplication.java
+++ trunk/tinywebgears/tinywebgears-jar/src/main/java/com/tinywebgears/webapp/WebApplication.java
@@ -1,29 +1,18 @@
 package com.tinywebgears.webapp;
 
-import java.io.Serializable;
-import java.security.Principal;
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.URL;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.tinywebgears.simplegae.core.dao.UserRepository;
-import com.tinywebgears.simplegae.core.model.User;
-import com.tinywebgears.simplegae.core.service.DefaultUserServices;
-import com.tinywebgears.simplegae.core.service.UserServicesIF;
 import com.vaadin.Application;
 import com.vaadin.service.ApplicationContext;
 import com.vaadin.terminal.ExternalResource;
-import com.vaadin.terminal.Resource;
 import com.vaadin.terminal.Sizeable;
-import com.vaadin.terminal.gwt.server.WebApplicationContext;
+import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Link;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Window.Notification;
@@ -34,19 +23,12 @@ public class WebApplication extends Application implements ApplicationContext.Tr
 
     private final Logger logger = LoggerFactory.getLogger(WebApplication.class);
 
-    private final UserServicesIF userServices;
-    private final UserSession session = new UserSession();
-    private Boolean initialized = false;
     private Window mainWindow;
     private HorizontalLayout headerLayout;
-    private HorizontalLayout loginForm;
-    private HorizontalLayout logoutForm;
-    private VerticalLayout userInfoLayout;
-    private Label emailLabel;
+    private VerticalLayout mainLayout;
 
     public WebApplication()
     {
-        userServices = new DefaultUserServices(DatabaseServices.userRepo);
     }
 
     /**
@@ -66,8 +48,7 @@ public class WebApplication extends Application implements ApplicationContext.Tr
         if (application == WebApplication.this)
         {
             currentApplication.set(this);
-            // Check the user's login status based on servlet request parameters.
-            checkLoginLogoutRequest((HttpServletRequest) servletRequest);
+            initUiComponents();
         }
     }
 
@@ -89,86 +70,6 @@ public class WebApplication extends Application implements ApplicationContext.Tr
         super.close();
     }
 
-    public UserServicesIF getUserServices()
-    {
-        return userServices;
-    }
-
-    private void checkLoginLogoutRequest(HttpServletRequest request)
-    {
-        logger.trace("checkLoginLogoutRequest - initialized? " + initialized);
-        Principal userPrincipal = request.getUserPrincipal();
-
-        if (initialized
-                && ((userPrincipal != null && session.getUsername() != null) || (userPrincipal == null && session
-                        .getUsername() == null)))
-        {
-            return;
-        }
-
-        UserService userService = UserServiceFactory.getUserService();
-
-        if (mainWindow == null)
-        {
-            logger.debug("Creating main window");
-            Resource loginUrl = new ExternalResource(userService.createLoginURL(request.getRequestURI()));
-            Resource logoutUrl = new ExternalResource(userService.createLogoutURL(request.getRequestURI()));
-            initUiComponents(loginUrl, logoutUrl);
-            setLoggedIn(null);
-        }
-
-        if (request.getUserPrincipal() != null)
-        {
-            // Login information is provided in the request.
-            String userid = request.getUserPrincipal().getName();
-            String nickname = userService.getCurrentUser().getNickname();
-            logger.info("User logged in: " + userid + " nick name: " + nickname + " admin: "
-                    + userService.isUserAdmin());
-            session.setSession(userid, userid, userService.isUserAdmin(), nickname);
-            try
-            {
-                User user = userServices.setUser(userid, userid, nickname);
-                setLoggedIn(user);
-            }
-            catch (Exception e)
-            {
-                logger.error("Unable to log in: " + e);
-                showMessage("Unable to log you in: " + e.getLocalizedMessage());
-            }
-        }
-        else
-        {
-            // No user information is provided in the request.
-            if (session.getUsername() != null)
-            {
-                // Logout is requested.
-                logger.info("User logged out: " + session.getUsername());
-                showMessage("You successfully logged out.");
-                try
-                {
-                    userServices.clearUser();
-                    session.clearSession();
-                    setLoggedIn(null);
-                }
-                catch (Exception e)
-                {
-                    logger.warn("Error occured while logging out: " + e);
-                    showMessage("Couldn't log you out: " + e.getLocalizedMessage());
-                }
-                finally
-                {
-                    ((WebApplicationContext) getContext()).getHttpSession().invalidate();
-                    // close();
-                }
-            }
-            else
-            {
-                // Application is initialized without login info.
-                logger.debug("Application reloaded");
-            }
-        }
-    }
-
     /**
      * This method is used by internal components to access global resources/services.
      * 
@@ -179,24 +80,19 @@ public class WebApplication extends Application implements ApplicationContext.Tr
         return currentApplication.get();
     }
 
-    public UserSession getUserSession()
-    {
-        return session;
-    }
-
     public void showMessage(String description)
     {
         if (mainWindow != null)
             mainWindow.showNotification("", description, Notification.TYPE_HUMANIZED_MESSAGE);
     }
 
-    private void initUiComponents(Resource loginUrl, Resource logoutUrl)
+    private void initUiComponents()
     {
         mainWindow = new Window("Simple GAE Application using Vaadin");
         mainWindow.setSizeFull();
         setMainWindow(mainWindow);
 
-        VerticalLayout mainLayout = new VerticalLayout();
+        mainLayout = new VerticalLayout();
         mainLayout.setMargin(true);
         mainLayout.setSpacing(true);
         mainLayout.setSizeFull();
@@ -204,62 +100,25 @@ public class WebApplication extends Application implements ApplicationContext.Tr
 
         headerLayout = new HorizontalLayout();
         headerLayout.setWidth(100, Sizeable.UNITS_PERCENTAGE);
+        headerLayout.addComponent(new Label("Tuatara Addressbook"));
         mainLayout.addComponent(headerLayout);
 
-        loginForm = new HorizontalLayout();
-        // loginForm.setWidth(100, Sizeable.UNITS_PERCENTAGE);
-        loginForm.setSpacing(true);
-        Label loginLabel = new Label("Please use this button to log in.");
-        loginLabel.setWidth(100, Sizeable.UNITS_PERCENTAGE);
-        loginForm.addComponent(loginLabel);
-        loginForm.addComponent(new Link("Login", loginUrl));
-
-        logoutForm = new HorizontalLayout();
-        // logoutForm.setWidth(100, Sizeable.UNITS_PERCENTAGE);
-        logoutForm.setSpacing(true);
-        emailLabel = new Label("");
-        emailLabel.setWidth(100, Sizeable.UNITS_PERCENTAGE);
-        logoutForm.addComponent(emailLabel);
-        logoutForm.addComponent(new Link("Logout", logoutUrl));
-
-        userInfoLayout = new VerticalLayout();
-        mainLayout.addComponent(userInfoLayout);
-        initialized = true;
-    }
-
-    private void setLoggedIn(User user)
-    {
-        headerLayout.removeAllComponents();
-        userInfoLayout.removeAllComponents();
-        if (user == null)
+        try
         {
-            headerLayout.addComponent(loginForm);
+            loadTuatara();
         }
-        else
+        catch (IOException e)
         {
-            headerLayout.addComponent(logoutForm);
-            emailLabel.setValue(user.getEmail());
-            Label welcomeLabel = new Label("Welcome " + user.getNickname());
-            welcomeLabel.setWidth(100, Sizeable.UNITS_PERCENTAGE);
-            userInfoLayout.addComponent(welcomeLabel);
-            Label lastLoginLabel = new Label();
-            if (user.getLastLoginDate() != null)
-                lastLoginLabel.setValue("Your last login time was " + user.getLastLoginDate());
-            else
-                lastLoginLabel.setValue("This is the first time you log in.");
-            lastLoginLabel.setWidth(100, Sizeable.UNITS_PERCENTAGE);
-            userInfoLayout.addComponent(lastLoginLabel);
-            userServices.updateUserLoginDate(user.getKey(), new Date());
+            showMessage("An exception occurred while displaying Tuatara Addressbook: " + e.getLocalizedMessage());
         }
     }
 
-    private static class DatabaseServices implements Serializable
+    private void loadTuatara() throws IOException
     {
-        private final static UserRepository userRepo;
-
-        static
-        {
-            userRepo = new UserRepository();
-        }
+        URL url = new URL("http://tuatara-addressbook.appspot.com/");
+        Embedded browser = new Embedded("", new ExternalResource(url));
+        browser.setType(Embedded.TYPE_BROWSER);
+        browser.setSizeFull();
+        mainLayout.addComponent(browser);
     }
 }
