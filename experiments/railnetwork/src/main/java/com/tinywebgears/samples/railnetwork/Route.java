c97f4cc3faf5f30ee901a74e9d99c6cb54aa28a1
houman001 <houman001@gmail.com>
2011-11-16 23:33:25 +0000

    Renamed package name

---
+++ experiments/railnetwork/src/main/java/com/tinywebgears/samples/railnetwork/Route.java
@@ -0,0 +1,43 @@
+package com.tinywebgears.samples.railnetwork;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Route
+{
+    private Integer totalDistance;
+    private List<StationNode> stations;
+
+    Route(StationNode sourceStation)
+    {
+        stations = new ArrayList<StationNode>();
+        stations.add(sourceStation);
+        totalDistance = 0;
+    }
+
+    void addStation(StationNode nextStation, Integer distance)
+    {
+        stations.add(nextStation);
+        totalDistance += distance;
+    }
+
+    Queue<String> getPath()
+    {
+        Queue<String> path = new LinkedList<String>();
+        for (StationNode station : stations)
+            path.offer(station.getName());
+        return path;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder path = new StringBuilder();
+        for (StationNode station : stations)
+            path.append(station.getName()).append("-");
+        path.append(totalDistance);
+        return path.toString();
+    }
+}
